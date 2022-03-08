b496d91bc95f14a54fb54a940d756c3f83db6895
luisacosta <luisacosta828@gmail.com>
2022-03-08 14:35:40 -0400

    added getStableCoinsInWallet.

--- src/binance.nim
+++ src/binance.nim
@@ -290,6 +290,11 @@ proc userWallet*(self: var Binance, update:bool = false): self.balances.type =
   self.balances
 
 
+proc getStableCoinsInWallet(self: var Binance): seq[tuple[coin: string, free: float]] =
+  var myWallet = self.userWallet(update = true)
+  for coin in myWallet.keys:
+    if coin in stableCoins:
+      result.add ( coin, myWallet[coin].free )
 
 
 proc verifyFiltersRule(self:Binance, symbol: string, price, quantity:float, tipe: OrderType):bool =
