070dfa3311d2fcbd6a092b22fa05e33699434d19
Juan Carlos <juancarlospaco@gmail.com>
2022-03-17 18:18:42 -0700

    Create newcoin.nim
    Add Tradingbot example.

---
+++ examples/newcoin.nim
@@ -0,0 +1,85 @@
+## TradingBot that buys new coins listed on Binance, as soon as they are available, at a cheap price, and sell them when they are pumped up in price.
+import std/[os, json, strutils, httpcore, math], binance
+
+
+proc main() =
+
+  let
+    client = newBinance(getEnv"BINANCE_API_KEY", getEnv"BINANCE_API_SECRET")
+    exchangeData = parseJson(client.exchangeInfo(fromMemory = true))["symbols"]
+
+  var
+    i = 0
+    currentCryptos: JsonNode
+    currentLen, newCoin: int
+    amount, priceToBuy: float
+    symbolToBuy, symbol: string
+    message = newStringOfCap(6)
+    prevCryptos = parseJson(client.request(client.tickerPrice()))
+    prevLen = prevCryptos.len
+
+  doAssert prevLen > 1956, "prevLen must be a positive integer greater than 1956."
+
+  while on:
+    currentCryptos = parseJson(client.request(client.tickerPrice()))
+    currentLen = currentCryptos.len
+    message.addInt prevLen
+    echo message
+    message.setLen 0
+
+    if prevLen < currentLen:
+      newCoin = prevLen
+      echo currentCryptos[newCoin]
+      break
+    else:
+      sleep 10_000
+
+  for indx in newCoin ..< currentLen:
+    symbol = currentCryptos[indx]["symbol"].getStr
+    doAssert symbol.len > 1, "symbol must not be empty string"
+
+    for asset in exchangeData:
+      if asset["symbol"].getStr == symbol:
+        symbolToBuy = asset["quoteAsset"].getStr
+        break
+
+    doAssert symbolToBuy.len > 0, "symbolToBuy is too short or empty string"
+
+    priceToBuy = currentCryptos[i]["price"].getStr.parseFloat
+    checkFloat priceToBuy
+    doAssert priceToBuy > 0.0, "priceToBuy is Negative or Zero"
+
+    case symbolToBuy
+    of "BTC" : amount = 0.0013
+    of "ETH" : amount = 0.03
+    of "USDT": amount = 15.0
+    of "BUSD": amount = 15.0
+    of "BNB" : amount = 0.7
+    else: echo "symbolToBuy not in 'BTC', 'ETH', 'USDT', 'BUSD', 'BNB'."
+
+    if amount == 0.0: continue
+    elif amount > 0.0:
+      inc i
+      echo client.request(client.postOrder(
+        symbol   = symbol,
+        side     = SIDE_BUY,
+        quantity = client.truncate(amount / priceToBuy, 3),
+        price    = client.truncate(priceToBuy, 3),
+        tipe     = ORDER_TYPE_LIMIT,
+      ), HttpPost)
+
+      echo client.request(client.newOrderOco(
+        symbol               = symbol,
+        side                 = SIDE_SELL,
+        quantity             = client.truncate(amount / priceToBuy, 3),
+        price                = client.truncate(priceToBuy * 1.020, 3),
+        stopPrice            = client.truncate(priceToBuy * 0.995, 3),
+        stopLimitPrice       = client.truncate(priceToBuy * 0.994, 3),
+        stopLimitTimeInForce = $TIME_IN_FORCE_GTC
+      ), HttpPost)
+    sleep 10_000
+  client.close()
+
+
+when isMainModule:
+  main()
