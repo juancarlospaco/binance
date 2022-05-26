1af721c93d11f78b0bb5f38141e7c58a8925ebf3
Juan Carlos <juancarlospaco@gmail.com>
2022-05-26 19:26:01 -0300

    updates

--- examples/example.nim
+++ examples/example.nim
@@ -1,5 +1,5 @@
 from std/os import getEnv
-import std/httpcore
+import std/[httpcore, json]
 import binance
 
 let client = newBinance(getEnv"BINANCE_API_KEY", getEnv"BINANCE_API_SECRET")
@@ -13,48 +13,48 @@ echo client.request(preparedEndpoint, HttpPost)
 
 
 #postOrder -> api/v3/order POST
-echo "\nPOST -> api/v3/order"
-preparedEndpoint = client.postOrder(SIDE_BUY, ORDER_TYPE_LIMIT, $TIME_IN_FORCE_GTC, "BNBUSDT", 0.01, 100.00)
-echo client.request(preparedEndpoint, HttpPost)
+# echo "\nPOST -> api/v3/order"
+# preparedEndpoint = client.postOrder(SIDE_BUY, ORDER_TYPE_LIMIT, $TIME_IN_FORCE_GTC, "BNBUSDT", 0.01, 100.00)
+# echo client.request(preparedEndpoint, HttpPost)
 
 
 #getOrder -> api/v3/order GET
 echo "\nGET -> api/v3/order"
 preparedEndpoint = client.getOrder("BTCUSDT")
-echo client.request(preparedEndpoint)
+echo client.request(preparedEndpoint, HttpGet)
 
 
 #GET -> api/v3/account
 echo "\nGET -> api/v3/account"
 preparedEndpoint = client.accountData()
-echo client.request(preparedEndpoint)
+echo client.request(preparedEndpoint, HttpGet)
 
 
 #GET -> api/v3/myTrades
 echo "\nGET -> api/v3/myTrades"
 preparedEndpoint = client.myTrades("BTCUSDT")
-echo client.request(preparedEndpoint)
+echo client.request(preparedEndpoint, HttpGet)
 
 
 #GET -> api/v3/rateLimit/order
 echo "\nGET -> api/v3/rateLimit/order"
 preparedEndpoint = client.rateLimitOrder()
-echo client.request(preparedEndpoint)
+echo client.request(preparedEndpoint, HttpGet)
 
 
 #GET -> api/v3/allOrderList
 echo "\nGET -> api/v3/allOrderList"
 preparedEndpoint = client.allOrderList()
-echo client.request(preparedEndpoint)
+echo client.request(preparedEndpoint, HttpGet)
 
 
 #GET -> api/v3/allOrderList
 echo "\nGET -> api/v3/openOrderList"
 preparedEndpoint = client.openOrderList()
-echo client.request(preparedEndpoint)
+echo client.request(preparedEndpoint, HttpGet)
 
 
 #GET -> api/v3/orderList
 echo "\nGET -> api/v3/orderList"
 preparedEndpoint = client.orderList(1)
-echo client.request(preparedEndpoint)
+echo client.request(preparedEndpoint, HttpGet)
