647742cab506f35dc483417b1a1b6703816dcff6
Juan Carlos <juancarlospaco@gmail.com>
2022-07-20 06:32:12 -0700

    Update binance.nim
    [skip ci]

--- src/binance.nim
+++ src/binance.nim
@@ -877,6 +877,18 @@ proc getBalanceFutures*(self: Binance; coin: string): float =
     if it["asset"].getStr == coin: return it["balance"].getStr.parseFloat
 
 
+proc hasOpenPositionsFutures*(self: Binance; ticker: string): bool =
+  ## Return `true` if `ticker` has open Positions. This is agnostic of open Orders. Works for Hedge OFF.
+  let position = self.request(self.positionRiskFutures(symbol = ticker), HttpGet)
+  if position.len == 1:
+    result = position[0]["entryPrice"].getStr != "0.0"
+
+
+proc hasOpenOrdersFutures*(self: Binance; ticker: string): bool =
+  ## Return `true` if `ticker` has open Orders. This is agnostic of open Positions. Works for Hedge OFF.
+  result = self.request(self.getAllOpenOrdersFutures(symbol = ticker), HttpGet).len > 0
+
+
 # User data streams ###########################################################
 
 
