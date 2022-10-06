2bfc21bcc404a7e31141411c7e3e85bd883b6f51
Juan_Carlos <juancarlospaco@gmail.com>
2022-10-06 17:47:56 -0300

    update

--- examples/bot/tradingbot_futures.nim
+++ examples/bot/tradingbot_futures.nim
@@ -23,7 +23,7 @@ proc main(config: JsonNode) =
   let
     startTime = now()
     walletBscProfits = config["wallet_address_bsc_to_send_profits"].getStr
-    minLeverage:              1 .. 15     = config["min_leverage"].getInt
+    minLeverage:              1 .. 20     = config["min_leverage"].getInt
     doNotTradeBelow:         50 .. 500    = config["do_not_trade_coins_below_usd"].getInt
     trailingStopLossOffset: 1.0 .. 5.0    = config["trailing_stop_loss_offset_percent"].getInt.float
     marketEntryOffset:    0.015 .. 0.9999 = config["price_offset_for_market_entry_order"].getFloat  # Do NOT use values lower than 0.015
